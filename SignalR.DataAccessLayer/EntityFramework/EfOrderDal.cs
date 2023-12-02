b16923ce70ca90f066516903e4a7f9c761e7ed8a
korog <korog@LAPTOP-GPMKKC5N>
2023-12-03 00:54:32 +0300

    SignalR istatistikler yapıldı.

--- SignalR.DataAccessLayer/EntityFramework/EfOrderDal.cs
+++ SignalR.DataAccessLayer/EntityFramework/EfOrderDal.cs
@@ -29,6 +29,19 @@ namespace SignalR.DataAccessLayer.EntityFramework
 			return c.Orders.OrderByDescending(x=>x.OrderID).Take(1).Select(y=>y.TotalPrice).FirstOrDefault();
 		}
 
+		public decimal TodayTotalPrice()
+		{
+			/*
+			 Bugünkü kazanç kısmı udemy soru-cevap bölümünden başka bir öğrencinin çözümü ile yapıldı.
+			 Hoca şimdilik es geçti, daha sonra dönülecek.
+			 */
+
+			using var c = new SignalRContext();
+			DateTime nowDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+			return c.Orders.Where(x => x.Date == nowDate && x.Description == "Hesap Kapatıldı").Sum(y => y.TotalPrice);
+			//return c.Orders.Where(x => x.Date == DateTime.Parse(DateTime.Now.ToShortDateString())).Sum(y => y.TotalPrice);
+		}
+
 		public int TotalOrderCount()
 		{
 			using var c = new SignalRContext();
--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -57,6 +57,12 @@ builder.Services.AddScoped<IOrderDal, EfOrderDal>();
 builder.Services.AddScoped<IOrderDetailService, OrderDetailManager>();
 builder.Services.AddScoped<IOrderDetailDal, EfOrderDetailDal>();
 
+builder.Services.AddScoped<IMoneyCaseService, MoneyCaseManager>();
+builder.Services.AddScoped<IMoneyCaseDal, EfMoneyCaseDal>();
+
+builder.Services.AddScoped<IMenuTableService, MenuTableManager>();
+builder.Services.AddScoped<IMenuTableDal, EfMenuTableDal>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
